ebc8276620836b4588aa685a8e08423691ac80f3
krish-1604 <krish1604mehta@gmail.com>
2023-09-28 15:48:35 +0530

    5th commit

--- lib/login.dart
+++ lib/login.dart
@@ -2,131 +2,147 @@ import 'package:first/home.dart';
 import 'package:first/signup.dart';
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
-  const Login({Key? key}) : super(key: key);
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String _Username = '';
+  String _Password = '';
+  final GlobalKey<FormState> _Formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF040404),
-      body: ListView(
-        children: [
-          Align(
-            alignment: Alignment.topLeft,
-            child: IconButton(
-              onPressed: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => Signup()));
-              },
-              icon: Icon(
-                Icons.arrow_back,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          SizedBox(height: 90),
-          Image.asset(
-            'assets/logo.png',
-            width: 150,
-            height: 150,
-          ),
-          SizedBox(height: 40),
-          Center(
-            child: Text(
-              "Happening now!",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 40,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          SizedBox(height: 10),
-          Center(
-            child: Text(
-              "Join today.",
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'Lato',
-                fontSize: 25,
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 16.0),
-            decoration: BoxDecoration(
-              color: Color(0xFF040404),
-              borderRadius: BorderRadius.circular(10.0),
-              border: Border.all(
-                color: Colors.blue,
-                width: 2.0,
-              ),
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Username',
-                hintStyle: TextStyle(color: Color(0xFF5A5A5A)),
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.all(16.0),
-              ),
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-          SizedBox(height: 20),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 16.0),
-            decoration: BoxDecoration(
-              color: Color(0xFF040404),
-              borderRadius: BorderRadius.circular(10.0),
-              border: Border.all(
-                color: Colors.blue,
-                width: 2.0,
-              ),
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Password',
-                hintStyle: TextStyle(color: Color(0xFF5A5A5A)),
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.all(16.0),
-              ),
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
+      body: Form(
+        key: _Formkey,
+        child: ListView(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: IconButton(
                 onPressed: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => Home()));
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Signup()));
                 },
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: 90),
+            Image.asset(
+              'assets/logo.png',
+              width: 150,
+              height: 150,
+            ),
+            SizedBox(height: 40),
+            Center(
+              child: Text(
+                "Happening now!",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: Text(
+                "Join today.",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Lato',
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16.0),
+              decoration: BoxDecoration(
+                color: Color(0xFF040404),
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(
+                  color: Colors.blue,
+                  width: 2.0,
+                ),
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Username',
+                  hintStyle: TextStyle(color: Color(0xFF5A5A5A)),
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.all(16.0),
+                ),
+                style: TextStyle(color: Colors.white),
+                onSaved: (value) {
+                  _Username = value ?? '';
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16.0),
+              decoration: BoxDecoration(
+                color: Color(0xFF040404),
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(
+                  color: Colors.blue,
+                  width: 2.0,
+                ),
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: TextStyle(color: Color(0xFF5A5A5A)),
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.all(16.0),
+                ),
+                style: TextStyle(color: Colors.white),
+                onSaved: (value) {
+                  _Password = value ?? '';
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Home()));
+                  },
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
                     ),
                   ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
-          ),
-        ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
